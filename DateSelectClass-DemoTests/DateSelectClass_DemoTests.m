eb94a418d7d5621eded586d7b7e6dc7b71232df9
王博 <wangbo@wangbodeMacBook-Pro.local>
2019-05-31 16:43:55 +0800

    时间类第一次上传

---
+++ DateSelectClass-DemoTests/DateSelectClass_DemoTests.m
@@ -0,0 +1,37 @@
+//
+//  DateSelectClass_DemoTests.m
+//  DateSelectClass-DemoTests
+//
+//  Created by 王博 on 2019/5/31.
+//  Copyright © 2019 wbgit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DateSelectClass_DemoTests : XCTestCase
+
+@end
+
+@implementation DateSelectClass_DemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
